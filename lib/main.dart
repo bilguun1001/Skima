6f151cd2ddf3852a7c59ae11fe671ea2a3a5d944
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-03-31 01:30:38 +0800

    'click'

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,51 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
+void main() {
+  runApp(MyApp());
+}
 
-  // Fields in a Widget subclass are always marked "final".
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
 
-  final Widget title;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: BodyWidget(),
+      ),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  final fruitNotfier = ValueNotifier<Color>(Colors.purple.shade600);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        // <Widget> is the type of items in the list.
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.purple.shade700,
+      child: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Text('Bilguun'),
+            SizedBox(height: 100),
+            ElevatedButton(
+              onPressed: () {
+                print('I was clicked.');
+
+                fruitNotfier.value = Colors.blue.shade200;
+              },
+              child: Text('Click me'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    title: 'My app', // used by the OS task switcher
-    home: SafeArea(
-      child: MyScaffold(),
-    ),
-  ));
-}
