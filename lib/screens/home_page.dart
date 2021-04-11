d03437e53d8278ffc2f23aadf532ee3869300f80
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-04-12 03:25:05 +0800

    change

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,81 +1,156 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const HomePage());
-
-/// This is the main application widget.
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Positioned(
+          bottom: height /70,
+          child: Image.network(
+            'https://images.squarespace-cdn.com/content/v1/56943972df40f384188df846/1598296611455-6UGGJ3K07JD6CRZA3DNS/ke17ZwdGBToddI8pDm48kNPKVmbt05aEWnErXou3fDl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0tb-hnCqoepq4X8c1traqO_6-8vaS3UGENu9QP5pfFlLbyLeIY6QzmBTG9h7XCKkkQ/lebron_james_by_maggie_west_1.jpg?format=2500w',
+            height: height,
+            width: width,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Foreground()
+      ],
     );
   }
 }
 
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'SKIMA',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Schedule',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Teams',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class Foreground extends StatelessWidget {
+  const Foreground({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    var inputBorder = OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
+      borderRadius: BorderRadius.all(
+        Radius.circular(30.0),
+      ),
+    );
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today_outlined),
-            label: 'Schedule',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people_alt),
-            label: 'Teams',
-          ),
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://scontent.fuln1-1.fna.fbcdn.net/v/t1.6435-9/116873907_1650015401824052_3431434889563061910_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=174925&_nc_ohc=JpPiF5s-2zkAX9hju8N&_nc_ht=scontent.fuln1-1.fna&oh=c69407a401f56ab1d8b9f02b28c5bd41&oe=6097DF34',
+                ),
+                backgroundColor: Colors.black12
+            ),
+            onPressed: () {},
+          )
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.purple.shade700,
-        onTap: _onItemTapped,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: DefaultTextStyle(
+          style: GoogleFonts.raleway(color: Colors.white),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 50),
+              Text(
+                'Hello Guys!',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Check the players informations',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 35),
+              TextField(
+                decoration: InputDecoration(
+                  border: inputBorder,
+                  enabledBorder: inputBorder,
+                  focusedBorder: inputBorder,
+                  hintText: 'Search players',
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  suffixIcon: Icon(Icons.search, color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 150),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text(
+                  'Players',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ]),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  for (var location in locations)
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: [
+                          ColorFiltered(
+                            colorFilter: ColorFilter.mode(
+                              Colors.black12,
+                              BlendMode.darken,
+                            ),
+                            child: Image.network(
+                              location.imageUrl,
+                              height: height * 0.35,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class Location {
+  final String imageUrl;
+
+  Location({
+    this.imageUrl,
+  });
+}
+
+final locations = [
+  Location(
+    imageUrl: 'https://mcdn.wallpapersafari.com/medium/43/44/dPtber.jpg',
+  ),
+  Location(
+    imageUrl: 'https://cdn.wallpapersafari.com/85/92/FIrOk1.png',
+  ),
+];
