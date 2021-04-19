ce1d72dc49e6db7bea1ef26b3ff1f94c4538ee49
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-04-19 11:32:16 +0800

    pls last

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:transformer_page_view/transformer_page_view.dart';
 
 import '../models/players_info.dart';
