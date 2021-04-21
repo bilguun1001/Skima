9aaea61f83c8ccc1a6f8d791141be1add68e8059
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-04-21 13:24:22 +0800

    give up

--- android/app/src/main/kotlin/com/example/skima/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/skima/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.skima
+package com.bilguun.skima
 
 import io.flutter.embedding.android.FlutterActivity
 
--- appname/android/app/src/main/kotlin/com/appname/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.appname
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
